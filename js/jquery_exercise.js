21402f5cd29188032fe6d5721df70439685378e0
Nic Tipton <nictipton@Nics-MacBook-Air.local>
2019-04-23 12:18:05 -0500

    Updating excercise

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -6,12 +6,30 @@
 
 //var myparagraph = $('#myPara').html();
 //alert(myparagraph);
-//
+
+//      Alternate way to call an alert (below)
+
+//alert($("#myPara").html());
+
 //var mylist = $('#myUL').html();
 //alert(mylist);
 
 //$(".codeup").css("border", "1px solid red");
 
-$("li").css("font-size", "20px");
+//$("li").css("font-size", "20px");
 
-$("h1, p, li").css("background-color", "yellow");
+//$("h1, p, li").css("background-color", "yellow");
+
+$('h1').click(function() {
+    $(this).css('background-color', 'yellow');
+});
+
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(function() {
+    $(this).css('color', 'red');
+}, function() {
+    $(this).css('color', 'black');
+});
