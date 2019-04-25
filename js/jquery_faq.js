37bf6407b3632e903426e5f02af5513acd5cd55a
Nic Tipton <nictipton@Nics-MacBook-Air.local>
2019-04-25 10:49:16 -0500

    Updating for effects

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -11,16 +11,34 @@
 
 
 
-$('h3').click(function(){
-    $(this).next().children().css('font-weight', 'bold')
+//$('h3').click(function(){
+//    $(this).next().children().css('font-weight', 'bold')
+//});
+//
+//$('#highlight').on('click', function() {
+//    $('ul').each(function(i,e){
+//        $(this).children().last().css('background', 'yellow')
+//    })
+//});
+//
+//$('li').on('click', function(){
+//    $(this).parent().children().first().css('color','blue')
+//});
+
+$('span').click(function() {
+    $('div').hide();
 });
 
-$('#highlight').on('click', function() {
-    $('ul').each(function(i,e){
-        $(this).children().last().css('background', 'yellow')
-    })
+$('.yellowstone').click(function() {
+    $('.yellowstones').slideToggle(2500);
 });
 
-$('li').on('click', function(){
-    $(this).parent().children().first().css('color','blue')
+$('.everglade').click(function() {
+    $('.everglades').slideToggle(2500);
 });
+
+$('.bigbend').click(function() {
+    $('.bigbends').slideToggle(2500);
+});
+
+$('h2').fadeIn(8000);
